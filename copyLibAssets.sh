5988569e900cf7e18a496293351ba1a48a302f5f
Sameer <sameer@cakesofttech.com>
2021-10-20 19:23:27 +0530

    Removed Old files

--- copyLibAssets.sh
+++
@@ -1,4 +0,0 @@
-echo "-------------------------------------------------"
-echo "Copying assets to library dist folder."
-echo "-------------------------------------------------"
-cp -avr ./projects/shopprop-avm/src/assets ./dist/shopprop-avm
--- projects/shopprop-avm/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of shopprop-avm
- */
-
-export * from './lib/shopprop-avm.service';
-export * from './lib/shopprop-avm.component';
-export * from './lib/shopprop-avm.module';
