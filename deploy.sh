3f3fd885bc28b1fad1f5311a8b7f349365196ffd
Sameer <sameer@cakesofttech.com>
2021-10-22 13:24:30 +0530

    deploy script Updated

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 rm -rf dist
 echo "Increment Version Inside products/app-avm-widget/package.json"
 npm install && npm run build
+npm list npm-cli-login || npm i npm-cli-login
 echo "Build Package To publish"
 echo -n Password: 
 read -s password
-npm-cli-login -u sameer-cakesoftech -p $password -e sameer@cakesofttech.com -r https://registry.npmjs.org/app-avm-widget
+npm-cli-login -u sameer-cakesoftech -p $password -e sameer@cakesofttech.com -r https://registry.npmjs.org/
 cd dist/app-avm-widget
 npm publish
 echo "success: Published package"
